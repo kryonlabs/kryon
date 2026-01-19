6209d51796188f22b51ffcf15d1b49ee3e35db88
waozixyz <waotzi@proton.me>
2026-01-18 23:31:55 -0300

    more cleanup

---
+++ runtime/desktop/ir_desktop_renderer.d
@@ -0,0 +1,70 @@
+ir_desktop_renderer.o: ir_desktop_renderer.c desktop_internal.h \
+ ../../core/include/kryon.h ../../core/include/../../ir/ir_platform.h \
+ ../../ir/ir_core.h ../../ir/ir_component_types.h \
+ ../../ir/ir_layout_types.h ../../ir/ir_style_types.h \
+ ../../ir/ir_properties.h ../../ir/ir_event_types.h ../../ir/ir_builder.h \
+ ../../ir/ir_core.h ../../ir/ir_animation.h ../../ir/ir_tabgroup.h \
+ ../../ir/ir_style_builder.h ../../ir/ir_layout_builder.h \
+ ../../ir/ir_event_builder.h ../../ir/ir_module_refs.h \
+ ../../ir/ir_table.h ../../ir/ir_markdown.h \
+ ../../ir/ir_animation_builder.h ../../ir/ir_gradient.h \
+ ../../ir/ir_hit_test.h ../../ir/ir_animation.h ../../ir/ir_transition.h \
+ ../../ir/ir_hot_reload.h ../../ir/ir_style_vars.h \
+ ../../ir/ir_serialization.h ../../ir/ir_buffer.h ir_desktop_renderer.h \
+ desktop_platform.h abstract/desktop_events.h abstract/desktop_fonts.h \
+ abstract/desktop_text.h abstract/desktop_effects.h desktop_test_events.h \
+ renderers/sdl3/sdl3_renderer.h renderers/sdl3/../../desktop_platform.h \
+ ../../core/include/kryon.h renderers/raylib/raylib_renderer.h \
+ renderers/raylib/../../desktop_internal.h ../../ir/ir_executor.h \
+ ../../ir/ir_logic.h ../../ir/ir_expression.h ir_to_commands.h \
+ ../../ir/ir_state_manager.h ../../ir/ir_executor.h \
+ ../../renderers/sdl3/sdl3.h \
+ ../../renderers/sdl3/../../core/include/kryon.h
+desktop_internal.h:
+../../core/include/kryon.h:
+../../core/include/../../ir/ir_platform.h:
+../../ir/ir_core.h:
+../../ir/ir_component_types.h:
+../../ir/ir_layout_types.h:
+../../ir/ir_style_types.h:
+../../ir/ir_properties.h:
+../../ir/ir_event_types.h:
+../../ir/ir_builder.h:
+../../ir/ir_core.h:
+../../ir/ir_animation.h:
+../../ir/ir_tabgroup.h:
+../../ir/ir_style_builder.h:
+../../ir/ir_layout_builder.h:
+../../ir/ir_event_builder.h:
+../../ir/ir_module_refs.h:
+../../ir/ir_table.h:
+../../ir/ir_markdown.h:
+../../ir/ir_animation_builder.h:
+../../ir/ir_gradient.h:
+../../ir/ir_hit_test.h:
+../../ir/ir_animation.h:
+../../ir/ir_transition.h:
+../../ir/ir_hot_reload.h:
+../../ir/ir_style_vars.h:
+../../ir/ir_serialization.h:
+../../ir/ir_buffer.h:
+ir_desktop_renderer.h:
+desktop_platform.h:
+abstract/desktop_events.h:
+abstract/desktop_fonts.h:
+abstract/desktop_text.h:
+abstract/desktop_effects.h:
+desktop_test_events.h:
+renderers/sdl3/sdl3_renderer.h:
+renderers/sdl3/../../desktop_platform.h:
+../../core/include/kryon.h:
+renderers/raylib/raylib_renderer.h:
+renderers/raylib/../../desktop_internal.h:
+../../ir/ir_executor.h:
+../../ir/ir_logic.h:
+../../ir/ir_expression.h:
+ir_to_commands.h:
+../../ir/ir_state_manager.h:
+../../ir/ir_executor.h:
+../../renderers/sdl3/sdl3.h:
+../../renderers/sdl3/../../core/include/kryon.h:
