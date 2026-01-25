9ca649e233f16f376d5ec465fde550bcc08b5cd4
waozixyz <waotzi@proton.me>
2026-01-24 21:34:56 -0300

    improve c codegen

--- codegens/c/ir_c_codegen.d
+++ codegens/c/ir_c_codegen.d
@@ -1,11 +1,13 @@
 ir_c_codegen.o: ir_c_codegen.c ir_c_codegen.h ir_c_internal.h \
  ../../third_party/cJSON/cJSON.h ir_c_output.h ir_c_types.h \
- ir_c_reactive.h ../codegen_common.h ../../ir/src/utils/ir_c_metadata.h
+ ir_c_reactive.h ir_c_expression.h ../codegen_common.h \
+ ../../ir/src/utils/ir_c_metadata.h
 ir_c_codegen.h:
 ir_c_internal.h:
 ../../third_party/cJSON/cJSON.h:
 ir_c_output.h:
 ir_c_types.h:
 ir_c_reactive.h:
+ir_c_expression.h:
 ../codegen_common.h:
 ../../ir/src/utils/ir_c_metadata.h:
