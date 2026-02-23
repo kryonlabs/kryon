012cc7312e6740ee700450e8deef3eb84b95ac18
waozixyz <waotzi@proton.me>
2026-02-22 22:12:11 -0300

    rm examples

--- examples/01-hello-world.lua
+++
@@ -1,6 +0,0 @@
--- Hello World Controller
--- Look up the greeting widget by ID
-local lbl = kryon.widget("lbl_greeting")
-
--- No events needed for static example
--- The label displays its text automatically
