e7b98a3e85355bbfcfc5f88b8830f1de9be3ee91
waozixyz <waotzi@proton.me>
2026-06-11 22:32:05 -0300

    Fix embedded asset lookup

--- src/flint_embedded_assets.c
+++ src/flint_embedded_assets.c
@@ -3,27 +3,28 @@
 #include <stdlib.h>
 #include <string.h>
 
-#if defined(__GNUC__)
-__attribute__((weak))
-#endif
-const FlintEmbeddedAsset flint_embedded_assets[] = {
-    {0, 0, 0, 0}
-};
-
-#if defined(__GNUC__)
-__attribute__((weak))
-#endif
-const unsigned int flint_embedded_asset_count = 0;
-
 static const char *
 normalize_path(const char *path)
 {
+    const char *match;
+
     if(path == NULL)
         return "";
     while(path[0] == '.' && path[1] == '/')
         path += 2;
     while(path[0] == '/')
         path++;
+
+    match = strstr(path, "/locales/");
+    if(match != NULL)
+        return match + 1;
+    match = strstr(path, "/themes/");
+    if(match != NULL)
+        return match + 1;
+    match = strstr(path, "/assets/");
+    if(match != NULL)
+        return match + 1;
+
     return path;
 }
 
@@ -34,7 +35,7 @@ flint_embedded_asset(const char *path)
 
     for(unsigned int i = 0; i < flint_embedded_asset_count; i++) {
         const FlintEmbeddedAsset *asset = &flint_embedded_assets[i];
-        if(asset->path != NULL && strcmp(asset->path, normalized) == 0)
+        if(asset->path != NULL && strcmp(normalize_path(asset->path), normalized) == 0)
             return asset;
     }
 
