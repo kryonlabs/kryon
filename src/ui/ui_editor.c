f77414e8dc213162c07698ac9c0c4535fd9f0416
Waozi <waozi@proton.me>
2026-07-19 00:27:41 -0300

    Make Flint editor hooks weak defaults

--- src/ui/ui_editor.c
+++ src/ui/ui_editor.c
@@ -1,34 +1,46 @@
 #include "ui_editor.h"
 
+#if defined(__GNUC__) || defined(__clang__)
+#define FLINT_EDITOR_WEAK __attribute__((weak))
+#else
+#define FLINT_EDITOR_WEAK
+#endif
+
+FLINT_EDITOR_WEAK
 void
 BeginUIEditorFrame(const char *project_root)
 {
     (void)project_root;
 }
 
+FLINT_EDITOR_WEAK
 void
 EndUIEditorFrame(void)
 {
 }
 
+FLINT_EDITOR_WEAK
 void
 SetUIEditorEnabled(int enabled)
 {
     (void)enabled;
 }
 
+FLINT_EDITOR_WEAK
 int
 UIEditorEnabled(void)
 {
     return 0;
 }
 
+FLINT_EDITOR_WEAK
 int
 UIEditorWidgetCount(void)
 {
     return 0;
 }
 
+FLINT_EDITOR_WEAK
 UIEditorSelection
 UIEditorGetSelection(void)
 {
@@ -36,12 +48,14 @@ UIEditorGetSelection(void)
     return selection;
 }
 
+FLINT_EDITOR_WEAK
 void
 SetUIEditorCanvasBounds(Rectangle bounds)
 {
     (void)bounds;
 }
 
+FLINT_EDITOR_WEAK
 int
 PushUIEditorChrome(int enabled)
 {
@@ -49,12 +63,14 @@ PushUIEditorChrome(int enabled)
     return 0;
 }
 
+FLINT_EDITOR_WEAK
 void
 PopUIEditorChrome(int token)
 {
     (void)token;
 }
 
+FLINT_EDITOR_WEAK
 int
 UIEditorInputCapturesClick(Vector2 point)
 {
@@ -62,6 +78,7 @@ UIEditorInputCapturesClick(Vector2 point)
     return 0;
 }
 
+FLINT_EDITOR_WEAK
 void
 UIEditorApplyBounds(const char *id, Rectangle *bounds)
 {
@@ -69,6 +86,7 @@ UIEditorApplyBounds(const char *id, Rectangle *bounds)
     (void)bounds;
 }
 
+FLINT_EDITOR_WEAK
 void
 UIEditorRegisterWidget(const char *id, const char *kind,
                        Rectangle *bounds, int flags)
@@ -79,6 +97,7 @@ UIEditorRegisterWidget(const char *id, const char *kind,
     (void)flags;
 }
 
+FLINT_EDITOR_WEAK
 void
 DrawUIEditorOverlay(void)
 {
