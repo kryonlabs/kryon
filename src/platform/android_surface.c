dbd4e46524be041b8db323a449c957dd5dbe6283
Waozi <waozi@proton.me>
2026-08-29 11:17:43 -0300

    Fix Android surface resize cache

--- src/platform/android_surface.c
+++ src/platform/android_surface.c
@@ -6,6 +6,7 @@
 
 #if defined(PLATFORM_ANDROID) || defined(__ANDROID__)
 #include <android/native_window.h>
+#include <stdbool.h>
 
 void rlLoadIdentity(void);
 void rlMatrixMode(int mode);
@@ -15,6 +16,34 @@ void rlViewport(int x, int y, int width, int height);
 
 #define KRY_RL_MODELVIEW 0x1700
 #define KRY_RL_PROJECTION 0x1701
+
+typedef struct { int x; int y; } KryRaylibCorePoint;
+typedef struct { unsigned int width; unsigned int height; } KryRaylibCoreSize;
+
+typedef struct KryRaylibCoreData {
+    struct {
+        const char *title;
+        unsigned int flags;
+        bool ready;
+        bool shouldClose;
+        bool resizedLastFrame;
+        bool eventWaiting;
+        bool usingFbo;
+        KryRaylibCoreSize display;
+        KryRaylibCoreSize screen;
+        KryRaylibCorePoint position;
+        KryRaylibCoreSize previousScreen;
+        KryRaylibCorePoint previousPosition;
+        KryRaylibCoreSize render;
+        KryRaylibCorePoint renderOffset;
+        KryRaylibCoreSize currentFbo;
+        KryRaylibCoreSize screenMin;
+        KryRaylibCoreSize screenMax;
+        Matrix screenScale;
+    } Window;
+} KryRaylibCoreData;
+
+extern KryRaylibCoreData CORE;
 #endif
 
 static int g_android_surface_w;
@@ -43,13 +72,30 @@ SyncAndroidSurfaceSize(int *width, int *height)
     ANativeWindow *window = (ANativeWindow *)GetWindowHandle();
     if(window != NULL) {
         int format = ANativeWindow_getFormat(window);
+        int changed;
 
         ANativeWindow_setBuffersGeometry(window, 0, 0, format);
         w = ANativeWindow_getWidth(window);
         h = ANativeWindow_getHeight(window);
         if(w > 0 && h > 0) {
+            changed = g_android_surface_w != w || g_android_surface_h != h ||
+                      (int)CORE.Window.screen.width != w ||
+                      (int)CORE.Window.screen.height != h ||
+                      (int)CORE.Window.render.width != w ||
+                      (int)CORE.Window.render.height != h;
             g_android_surface_w = w;
             g_android_surface_h = h;
+            CORE.Window.display.width = (unsigned int)w;
+            CORE.Window.display.height = (unsigned int)h;
+            CORE.Window.screen.width = (unsigned int)w;
+            CORE.Window.screen.height = (unsigned int)h;
+            CORE.Window.render.width = (unsigned int)w;
+            CORE.Window.render.height = (unsigned int)h;
+            if(!CORE.Window.usingFbo) {
+                CORE.Window.currentFbo.width = (unsigned int)w;
+                CORE.Window.currentFbo.height = (unsigned int)h;
+            }
+            CORE.Window.resizedLastFrame = changed;
             rlViewport(0, 0, w, h);
             rlMatrixMode(KRY_RL_PROJECTION);
             rlLoadIdentity();
