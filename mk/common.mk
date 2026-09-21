58c07cafa4e1d7f0093334cf40c5dcbfbca90bab
Waozi <waozi@proton.me>
2026-09-21 13:46:06 -0300

    Drop the deleted ui_icons.h as an icon rule prerequisite in mk/common.mk

--- mk/common.mk
+++ mk/common.mk
@@ -334,7 +334,7 @@ $(WEB_DIST_DIR): | $(BUILD_DIST_DIR)
 $(RAYLIB_BUILD_DIR): | build
 	mkdir -p $@
 
-$(KRYON_ICON_ASSETS_C) $(KRYON_ICON_NAMES_C) $(KRYON_ICON_TYPES_H): $(KRYON_ICON_FILES) $(KRYON_DIR)/scripts/embed-icon-sheets.py $(KRYON_DIR)/include/ui_icons.h | $(NATIVE_OBJ_DIR)
+$(KRYON_ICON_ASSETS_C) $(KRYON_ICON_NAMES_C) $(KRYON_ICON_TYPES_H): $(KRYON_ICON_FILES) $(KRYON_DIR)/scripts/embed-icon-sheets.py | $(NATIVE_OBJ_DIR)
 	python3 $(KRYON_DIR)/scripts/embed-icon-sheets.py "$(abspath $(KRYON_DIR)/icons)" "$(abspath $(KRYON_ICON_ASSETS_C))" \
 		--types-output "$(abspath $(KRYON_ICON_TYPES_H))" \
 		--names-output "$(abspath $(KRYON_ICON_NAMES_C))"
